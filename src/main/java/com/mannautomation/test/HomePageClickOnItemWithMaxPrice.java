5a2f93f8de679b63f6a3052abae7487b76203312
MannDheeraj <dheerajmann@autism360.com>
2023-12-29 13:21:33 +1100

    Removing unused imports

--- src/main/java/com/mannautomation/test/HomePageClickOnItemWithMaxPrice.java
+++ src/main/java/com/mannautomation/test/HomePageClickOnItemWithMaxPrice.java
@@ -1,11 +1,6 @@
 package com.mannautomation.test;
 
-import java.math.BigDecimal;
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import com.mannautomation.uicomponents.homePage.*;
 import com.mannautomation.base.BaseTest;

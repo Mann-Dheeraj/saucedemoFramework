47319624f9a5fa3bb095f377dbc43d47c0f7a987
MannDheeraj <dheerajmann@autism360.com>
2024-01-09 22:34:47 +1100

    Testng data provider implemented

--- src/main/java/com/mannautomation/test/LoginTest.java
+++ src/main/java/com/mannautomation/test/LoginTest.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertEquals;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.mannautomation.base.BaseTest;
@@ -11,17 +12,29 @@ import com.mannautomation.uicomponents.LoginPageFunctionality;
 public class LoginTest extends BaseTest {
 
 
-	@Test
-	public void loginWithEmailAndPassword() throws IOException {
+	@Test(dataProvider = "emailPasswordData")
+	public void loginWithEmailAndPassword(String uname, String pass) throws IOException {
 		LoginPageFunctionality lpf = new LoginPageFunctionality();
 		WebDriver driver = this.driver;
-		String usernameOfCustomer = "standard_user";
-		String passwordOfCustomer = "secret_sauce";
+		String usernameOfCustomer = uname;
+		String passwordOfCustomer = pass;
 
 		lpf.navigateToLoginPage(driver);
 		lpf.loginWithEmailAndPassword(driver,usernameOfCustomer,passwordOfCustomer);
-		boolean iSUrlCorrect= lpf.AssertLoginPageUrl("https://www.saucedemo.com/", driver);
+		boolean iSUrlCorrect= lpf.AssertLoginPageUrl("https://www.saucedemo.com/inventory.html", driver);
 		assertEquals(iSUrlCorrect, true);
+	}
+	
+	@DataProvider (name="emailPasswordData")
+	public Object[][] testDataGenerator() {
+		Object [][] emailPasswordCombination = {{"standard_user", "secret_sauce"},
+												{"locked_out_user", "secret_sauce"},
+												{"problem_user", "secret_sauce"},
+												{"performance_glitch_user", "secret_sauce"},
+												{"error_user", "secret_sauce"},
+												{"visual_user", "secret_sauce"}
+												};
+		return emailPasswordCombination;
 	}
 	
 }

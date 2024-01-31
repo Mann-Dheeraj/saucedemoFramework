e1855c36abe7c02d04578bca31152313d7908f26
MannDheeraj <dheerajmann@autism360.com>
2024-01-31 12:43:58 +0530

    Adding method to find item with max and min price and add them to cart

--- src/main/java/com/mannautomation/test/HomePageClickOnItemWithMaxPrice.java
+++ src/main/java/com/mannautomation/test/HomePageClickOnItemWithMaxPrice.java
@@ -1,6 +1,8 @@
 package com.mannautomation.test;
 
 import java.io.IOException;
+
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import com.mannautomation.base.BaseTest;
@@ -12,21 +14,22 @@ public class HomePageClickOnItemWithMaxPrice extends BaseTest {
 	LoginPageFunctionality lpf;
 	HomePageItemsFunctionality hpf;
 	
-	@BeforeTest
+	@BeforeMethod
 	public void setUPReference() {
 		lpf = new LoginPageFunctionality(this.driver);
 		hpf = new HomePageItemsFunctionality(this.driver);
 	}
 
 	@Test
-	public void findItemWithMaxPrice() throws IOException {
+	public void findItemWithMinAndMaxPrice() throws IOException {
 
 		String usernameOfCustomer = "standard_user";
 		String passwordOfCustomer = "secret_sauce";
 
 		lpf.navigateToLoginPage(driver);
 		lpf.loginWithEmailAndPassword(driver, usernameOfCustomer, passwordOfCustomer);
-		hpf.addItemWithMaxPriceToCart(driver);
+//		hpf.addItemWithMaxPriceToCart(driver);
+		hpf.findMaxAndMinPriceItemWithIndex();
 
 	}
 
